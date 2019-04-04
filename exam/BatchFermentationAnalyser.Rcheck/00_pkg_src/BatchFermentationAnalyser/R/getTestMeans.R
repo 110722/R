aceff5827d4f6d293e280dcc9e818c2d9aff270f
Claudia Haiderer <claudia.haiderer@gmail.com>
2019-04-04 15:04:45 +0200

    trying to build package

---
+++ exam/BatchFermentationAnalyser.Rcheck/00_pkg_src/BatchFermentationAnalyser/R/getTestMeans.R
@@ -0,0 +1,21 @@
+#' getTestMeans
+#'
+#' Auxiliary Function for getBatchMeans.
+#' @param list of dataframes
+#' @param na.rm
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' getTestMeans(a_wo_outlier)
+#'
+
+getTestMeans <- function(x, na.rm = T) {
+  for (i in 1:length(x)) {
+    x[[i]] <- apply(x[[i]], 2, mean, na.rm = na.rm)
+  }
+  return(x)
+}
+
+
---
+++ exam/BatchFermentationAnalyser/R/a.R
@@ -0,0 +1,19 @@
+#' a
+#'
+#' A list containing names of example datasets.
+#'
+#'  @format
+#'  A list of length 8.
+#'
+#'  @name
+#'  a
+NULL
+
+
+# basis_dir <- getwd()
+# input_dir <- paste(basis_dir, "/input/", sep = "")
+# output_dir <- paste(basis_dir, "/output/", sep = "")
+# a <- paste(input_dir, "data/A/A", 1:8, ".txt", sep="")
+# save(file="data/a.rda", list="a")
+
+
